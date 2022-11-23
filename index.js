9c3e5444db0c805d8195848aca4e3b5dcadbf2d9
yousufBD10 <yousufhassan.yh2@gmail.com>
2022-11-24 02:45:06 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -10,6 +10,11 @@ const port= process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.db_users}:${process.env.db_password}@cluster0.fvyg8ej.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
