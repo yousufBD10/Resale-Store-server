afbff34e1e02dbc676202080e699bd3a1d6ac21e
yousufBD10 <yousufhassan.yh2@gmail.com>
2022-11-25 21:58:09 +0600

    admin sellers buyers data to send client from db

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middle ware
 app.use(cors());
@@ -13,84 +13,104 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.db_users}:${process.env.db_password}@cluster0.fvyg8ej.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    //clirnt connect
+    const ProductsCollection = client
+      .db("ResaleStore")
+      .collection("ProductsData");
+    const usersCollection = client.db("ResaleStore").collection("users");
+    const blogCollection = client.db("ResaleStore").collection("blogs");
+    const bookingCollection = client.db("ResaleStore").collection("booking");
 
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await ProductsCollection.find(query).toArray();
 
-
-
-async function run(){
-    try{
-       //clirnt connect
-       const ProductsCollection = client.db('ResaleStore').collection('ProductsData');
-       const usersCollection = client.db('ResaleStore').collection('users');
-       const blogCollection = client.db('ResaleStore').collection('blogs');
-       const bookingCollection = client.db('ResaleStore').collection('booking');
-
-         
-       app.get('/categories', async (req,res)=>{
-        const query = {}
-        const categories =await  ProductsCollection.find(query).toArray();
-       
-        res.send(categories);
+      res.send(categories);
     });
-       app.get('/blog', async (req,res)=>{
-        const query = {}
-        const categories =await  blogCollection.find(query).toArray();
-       
-        res.send(categories);
+    app.get("/blog", async (req, res) => {
+      const query = {};
+      const categories = await blogCollection.find(query).toArray();
+
+      res.send(categories);
     });
 
-       app.get('/:brand', async (req,res)=>{
-        const brand = req.params.brand;
-        const query = {
-            brand:brand
-        }
-        const categorie =await  ProductsCollection.find(query).toArray();;
-       
-        res.send(categorie);
+    app.get("/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = {
+        brand: brand,
+      };
+      const categorie = await ProductsCollection.find(query).toArray();
+
+      res.send(categorie);
     });
-    
+
     // save user to db
-    
-    app.post('/users',async(req,res)=>{
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        res.send(result); 
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     });
 
+    // addmin
 
-    // booking data 
-    app.post('/booking',async(req,res)=>{
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result); 
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
     });
- 
 
-    app.get('/blog/:id', async (req,res)=>{
-        const id = req.params.id
-        const query = {_id:ObjectId(id)};
-        const singleBlog = await blogCollection.findOne(query);
-       
-        res.send(singleBlog);
+    // sellers 
+    app.get("/users/sellers/:email", async (req, res) => {
+        const email = req.params.email;
+        console.log(email);
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isSellers: user?.role === "seller" });
+      });
 
-     });
+        // buyers 
+    app.get("/users/buyers/:email", async (req, res) => {
+        const email = req.params.email;
+      
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isBuyers: user?.role === "buyer" });
+      });
 
-    }
-    catch{
-        console.error(error)
-    }
+    // booking data
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleBlog = await blogCollection.findOne(query);
 
+      res.send(singleBlog);
+    });
+  } catch {
+    console.error(error);
+  }
 }
-run().catch(err=>console.error(err))
+run().catch((err) => console.error(err));
 
+app.get("/", (req, res) => {
+  res.send("resale server is running");
+});
 
-app.get('/',(req,res)=>{
-    res.send('resale server is running');
-})
-
-app.listen(port,()=>{
-    console.log(`Resale server is running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Resale server is running on ${port}`);
+});
